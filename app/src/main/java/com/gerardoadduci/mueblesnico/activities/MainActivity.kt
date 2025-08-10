57505ecd59f67f28ca688183302ca2c3dddacd56
Gerard <95316097+GerardoAdduci@users.noreply.github.com>
2025-08-10 19:05:37 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/gerardoadduci/mueblesnico/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gerardoadduci.mueblesnico.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gerardoadduci.mueblesnico.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
